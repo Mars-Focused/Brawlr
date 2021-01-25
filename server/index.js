9cb77ead3e196438928b549f28a075426c3fb9c6
Mars_Clarke <marsthecoder@outlook.com>
2021-01-25 16:29:59 -0700

    testing logout

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(
 );
 
 app.post("/auth/register", authCtrl.register);
-// app.post("/auth/login", authCtrl.login);
-// app.get("/auth/logout", authCtrl.logout);
+app.post("/auth/login", authCtrl.login);
+app.get("/auth/logout", authCtrl.logout);
 
 app.listen(PORT, () => console.log(`Port ${PORT} Ready Master! `));
