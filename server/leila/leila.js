79246972f3eef88f4239408dc2798111fd039ffd
Mars_Clarke <marsthecoder@outlook.com>
2021-01-28 10:42:06 -0700

    start of the code-day

--- server/leila/leila.js
+++ server/leila/leila.js
@@ -1,101 +1,183 @@
 module.exports = {
   Fox: (port, name) => {
+    // <- Comment out these top to lines, AND the bottom two lines, to import them into your server index file.
+    // function Fox(port, name) {
+    // <- to start/stop testing ____________________________________________________________________________________________________________
     let frame = 0;
+    let phraseNumArr = [1, 2, 3, 4, 5, 6, 7, 8];
+    let phraseNumSelector = 0;
+
+    function shuffle(array) {
+      var m = array.length,
+        t,
+        i;
+      while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+      }
+      return array;
+    }
+    shuffle(phraseNumArr);
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     function leilaAnimation() {
       setTimeout(() => {
         leilaAnimation();
       }, 937.5);
+
       frame++;
       let leila = "";
-      if (frame > 700) {
-        frame = 701;
+      let phraseBit = "";
+
+      if (frame > 16) {
+        phraseNumSelector++;
+        frame = 9;
       }
 
+      phraseFrame = frame - 8;
+
+      if (phraseNumSelector >= phraseNumArr.length) {
+        shuffle(phraseNumArr);
+        phraseNumSelector = 0;
+      }
+
+      phraseNumber = phraseNumArr[phraseNumSelector];
+
+      // Connection ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      if (frame == 1) {
+        phraseBit = `Port ${port} Ready Sir!`;
+      }
+
+      if (frame == 2) {
+        phraseBit = `Database Connected Nya!`;
+      }
+
+      if (frame == 3) {
+        phraseBit = `Ready to Serve`;
+      }
+
+      if (frame == 4) {
+        phraseBit = `Master ${name}`;
+      }
+
+      /*
+    PHRASE REFERENCE COPY AND PAST THE 4 LINES BELOW TO START A PHRASE
+    // Phrase X ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    if (phraseFrame == frameNum && phraseNumber == phraseNum && phraseBit == "") {
+      phraseBit = `myPhrase`
+    }
+    */
+      // Phrase 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      if (phraseFrame == 1 && phraseNumber == 1 && phraseBit == "") {
+        phraseBit = `You're Doing Great`;
+      }
+
+      if (phraseFrame == 2 && phraseNumber == 1 && phraseBit == "") {
+        phraseBit = `Keep Coding Nya!`;
+      }
+
+      // Phrase 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      if (phraseFrame == 1 && phraseNumber == 2 && phraseBit == "") {
+        phraseBit = `Nya~ Nya~`;
+      }
+
+      if (phraseFrame == 2 && phraseNumber == 2 && phraseBit == "") {
+        phraseBit = `Purr~ Purr~`;
+      }
+
+      // Phrase 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      if (phraseFrame == 1 && phraseNumber == 3 && phraseBit == "") {
+        phraseBit = `I love you ${name}`;
+      }
+
+      if (phraseFrame == 4 && phraseNumber == 3 && phraseBit == "") {
+        phraseBit = `I hope that's okay...`;
+      }
+
+      // Phrase 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      if (phraseFrame == 1 && phraseNumber == 4 && phraseBit == "") {
+        phraseBit = `Do you Like...`;
+      }
+
+      if (phraseFrame == 2 && phraseNumber == 4 && phraseBit == "") {
+        phraseBit = `My Stockings...`;
+      }
+
+      if (phraseFrame == 3 && phraseNumber == 4 && phraseBit == "") {
+        phraseBit = `Master ${name}?`;
+      }
+
+      // Phrase 5 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      if (phraseFrame == 1 && phraseNumber == 5 && phraseBit == "") {
+        phraseBit = `Stop Staring...`;
+      }
+
+      if (phraseFrame == 2 && phraseNumber == 5 && phraseBit == "") {
+        phraseBit = `At My Boobs...`;
+      }
+
+      if (phraseFrame == 5 && phraseNumber == 5 && phraseBit == "") {
+        phraseBit = `Just Kidding!`;
+      }
+
+      if (phraseFrame == 6 && phraseNumber == 5 && phraseBit == "") {
+        phraseBit = `I Don't Mind!`;
+      }
+
+      // Phrase 6 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      if (phraseFrame == 1 && phraseNumber == 6 && phraseBit == "") {
+        phraseBit = `Work Hard`;
+      }
+
+      if (phraseFrame == 2 && phraseNumber == 6 && phraseBit == "") {
+        phraseBit = `Earn Money`;
+      }
+
+      if (phraseFrame == 3 && phraseNumber == 6 && phraseBit == "") {
+        phraseBit = `Raise CatGirls`;
+      }
+
+      //LEILA CREATION GOES DOWN HERE!!! AFTER DECIDING WHAT SHE SAYS IF ANYTHING.
       // head and text bubble
+      //IF phraseBit IS TRUE! THEN USE SPEAKING HEAD, ELSE NON-SPEAKING HEAD
+
       let leilaHead = "";
 
-      if (frame == 701) {
-        leilaHead += `(ZZZZZZZZZZZ)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
+      if (phraseBit && leilaHead == "") {
+        leilaHead = `(${phraseBit})\n  \\ |\\_/|    \n    |/ \\|   \n    *\\o/* \n`; //SPEAKING HEAD
+      } else {
+        leilaHead = `    .   .\n    |\\_/|    \n    |/ \\|   \n    *\\=/* \n`; //NON-SPEAKING HEAD
       }
 
-      if (frame == 1 && leilaHead == "") {
-        leilaHead += `(Pt ${port} OK!)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if (frame == 2 && leilaHead == "") {
-        leilaHead += `(DB OK! Nya!)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if (frame == 3 && leilaHead == "") {
-        leilaHead += `(Ready2Serve)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if (frame == 4 && leilaHead == "") {
-        leilaHead += `(Master ${name})\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if (frame % 32 == 0 && leilaHead == "") {
-        leilaHead += `(Doing Great)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if ((frame - 1) % 32 == 0 && leilaHead == "" && frame > 2) {
-        leilaHead += `(Keep Coding)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if ((frame - 7) % 23 == 0 && leilaHead == "") {
-        leilaHead += `(I'm a Fox!)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if (frame % 24 == 0 && leilaHead == "") {
-        leilaHead += `(Nyan~ Nyan~)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if ((frame - 1) % 24 == 0 && leilaHead == "" && frame > 2) {
-        leilaHead += `(Purr~ Purr~)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if (frame % 16 == 0 && leilaHead == "") {
-        leilaHead += `(I <3 Master)\n    |\\_/|  / \n    |/ \\|   \n    *\\o/* \n    .-"-. \n`;
-      }
-
-      if (leilaHead == "") {
-        leilaHead += `    .   .\n    |\\_/|    \n    |/ \\|   \n    *\\=/* \n    .-"-. \n`;
-      }
       leila += leilaHead;
 
-      // body, skirt, top of thighs
-      let leilaBody = "";
-
-      if (
-        leilaHead !=
-          `    .   .\n    |\\_/|    \n    |/ \\|   \n    *\\=/* \n    .-"-. \n` &&
-        leilaBody == ""
-      ) {
-        leilaBody += `   /(_Y_)\\ \n   \\\\):(//  \n    /___\\ \n    \\~|~/ \n`;
-      }
-
-      if (leilaBody == "") {
-        leilaBody += `   /(_Y_)\\ \n   \\\\):(// \n    /___\\ \n    \\~|~/ \n`;
-      }
+      // shoulders, body, skirt, top of thighs
+      let leilaBody = `    .-"-. \n   /(_Y_)\\ \n   \\\\):(// \n    /___\\ \n    \\~|~/ \n`;
       leila += leilaBody;
 
       // Legs and swishing tail
       let leilaLegs = "";
 
-      if (frame % 30 == 0 && leilaLegs == "") {
-        leilaLegs += `    /\\|/\\ \n/\\_/ /|\\ \\_/\\ \n\\___/\\|/\\___/ \n     /Y\\`;
-      }
-
       if (frame % 2 == 0 && leilaLegs == "") {
         leilaLegs += `    /\\|/ \n/\\_/ /|\\ \n\\___/\\|/ \n     /Y\\`;
-      }
-      if (leilaLegs == "") {
+      } else {
         leilaLegs = `     \\|/\\ \n     /|\\ \\_/\\ \n     \\|/\\___/ \n     /Y\\`;
       }
+
       leila += leilaLegs;
+
+      console.clear();
       console.log(leila);
+      //DEBUG LOGIC BELOW
+      // console.log("frame:", frame);
+      // console.log("phraseNumber:", phraseNumber);
+      // console.log("phraseFrame:", phraseFrame);
+      // __________________________________________________________________________________________________________________________________________
     }
     // Wrap the equation below with a setTimeout function to delay it, rather than starting the animation immediately
     leilaAnimation();
   },
+  // Fox(5000, "Mars");
 };
