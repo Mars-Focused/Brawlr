e741b577f81b262062f7f8fbfb0757043c6677cf
Mars_Clarke <marsthecoder@outlook.com>
2021-01-29 14:25:31 -0700

    doing stuff to pass down SPid's to SPBox'

--- db/get_rec_sp.sql
+++ db/get_rec_sp.sql
@@ -2,9 +2,6 @@
 --logged in user, minus the people who are already on his friends list.
 --we can cut it down to just get ALL peole who share a game with us
 
--- SELECT user_id FROM game_user_junction
--- WHERE game_id = $1
-
 SELECT DISTINCT user_id FROM game_user_junction
 WHERE game_id IN (
 SELECT game_id from game_user_junction
@@ -16,10 +13,3 @@ WHERE user_id_1 = $1 OR user_id_2 = $1
 SELECT user_id_1 FROM user_user_junction
 WHERE user_id_1 = $1 OR user_id_2 = $1
 )
-
---take in user id
---Find each row for our user_id in user_game_junction_table
---return all game id's connected to our user
---for game_id find each user_id
-
---DON'T BOTHER FILTERING OUT PEOPLE WE'RE ALREADY SP'S WITH!!!!!!!!!!!!
--- src/Components/MainSPList/MainSPList.js
+++ src/Components/MainSPList/MainSPList.js
@@ -1,22 +1,34 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { getRecSP } from "../../../server/controllers/sPartnerController";
+import SPBox from "../SPBox/SPBox";
+// import { getRecSP } from "../../../server/controllers/sPartnerController";
 
 //we need to call getRecSP from sPartnerController
 //and pass in User_id off of this state
 //it will return user_id objects within an array
 //we need to map each object into its own SPBox
 
-const MainSPList = (props) => {
-  function getSparingPartners(input) {
-    const { user_id } = props;
-    axios.get("/api/get_rec_sp", { user_id });
+const MainSPList = () => {
+  function getSparingPartners() {
+    axios.get("/api/get_rec_sp");
   }
-  getSparingPartners(props);
+
+  let CPArr = getSparingPartners();
+
   return (
     <div>
       <h1>MainSPList</h1>
+      <div className="sp_list">
+        {CPArr.map((SPid) => (
+          <SPBox SPid={SPid} />
+        ))}
+      </div>
+      {/* <div className="date_list">
+        {datezArr.map((date_idea) => (
+          <Date_Idea ideaNId={date_idea} />
+        ))}
+      </div> */}
     </div>
   );
 };
