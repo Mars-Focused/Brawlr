1cdf865c1aa1eda8320ade2834988a3dac060ac5
Mars_Clarke <marsthecoder@outlook.com>
2021-01-31 19:26:15 -0700

    still trying to get sass to work

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import "./Login.scss";
 import { connect } from "react-redux";
 import { loginUser } from "../../Redux/userReducer";
