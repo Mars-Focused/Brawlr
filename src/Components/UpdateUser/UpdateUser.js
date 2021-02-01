662d06b5280bcd1e87135911b248595d3d42fbf4
Mars_Clarke <marsthecoder@outlook.com>
2021-02-01 10:56:24 -0700

    imported stylemaster in all of my sass folders

--- src/Components/UpdateUser/UpdateUser.js
+++ src/Components/UpdateUser/UpdateUser.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import "./UpdateUser.scss";
 import AddContactInfo from "../AddContactInfo/AddContactInfo";
 import AddLikedGame from "../AddLikedGame/AddLikedGame";
 class UpdateUser extends Component {
   render() {
     return (
-      <div>
-        <h1>UpdateUserView</h1>
+      <div className="update-box">
+        <div className="text">Add Games and your Contact Info here!</div>
         <AddLikedGame {...this.props} />
         <AddContactInfo {...this.props} />
       </div>
